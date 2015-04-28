a82be3957888dad0be8101aabe6e7516b691ec10
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2015-04-28 22:22:18 +0200

    Update install-zookeeper.sh

--- install-zookeeper.sh
+++ install-zookeeper.sh
@@ -1,4 +1,4 @@
-tar xf /tmp/zookeeper-${ZOOKEEPER_VERSION}.tgz -C /opt
-mv /opt/zookeeper-3.4.6 /opt/zookeeper
+tar xf /tmp/zookeeper-${ZOOKEEPER_VERSION}.tar.gz -C /opt
+mv /opt/zookeeper-${ZOOKEEPER_VERSION} /opt/zookeeper
 cp /opt/zookeeper/conf/zoo_sample.cfg /opt/zookeeper/conf/zoo.cfg
 mkdir -p /tmp/zookeeper
