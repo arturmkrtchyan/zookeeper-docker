ed85b3609a8889ec726bcc3d713a3a47deca27bc
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2015-04-29 08:20:33 -0700

    Changed permissions and fixed jre install

--- install-jre.sh
+++ install-jre.sh
@@ -1,4 +1,5 @@
-add-apt-repository ppa:openjdk-r/ppa
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update 
 apt-get install -y openjdk-8-jre-headless
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
